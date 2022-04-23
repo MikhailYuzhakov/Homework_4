90b380da7e39a1bea9641c7d49242de1ee1bf6c8
MikhailYuzhakov <miha.yuzhakov@gmail.com>
2022-04-23 22:00:37 +0300

    возводит а в степень б

---
+++ Task_1/Program.cs
@@ -0,0 +1,18 @@
+﻿/*
+Задача 25: Напишите (функцию) цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+*/
+
+Console.Clear();
+Console.WriteLine("Введите число и степень в которую его необходимо возвести: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int pow = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine(number + " в " + pow + " степени = " + userPow(number, pow));
+
+static int userPow(int numb, int deg) {
+    int res = 1;
+    for (int i = 1; i <= deg; i++) {
+        res = res * numb;
+    }
+    return res;
+}
